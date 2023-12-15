54cfcab843362e23aec7e9d68022e9268a298e03
SkiletCool-desing <128517776+SkiletCool-desing@users.noreply.github.com>
2023-12-15 10:24:59 +0200

    Update ContactList.jsx

--- src/components/contactList/ContactList.jsx
+++ src/components/contactList/ContactList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 
 export const ContactList = () => {
   const contacts = useSelector((state) => state.contacts.contacts);
