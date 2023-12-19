31bb63b1c540fab6c0d67a4902a0e6e8ff219597
SkiletCool-desing <128517776+SkiletCool-desing@users.noreply.github.com>
2023-12-19 18:45:39 +0200

    phone

--- src/redux/contactsReducer.js
+++ src/redux/contactsReducer.js
@@ -1,48 +1,12 @@
-import {
-  reqestAddContact,
-  reqestAllContacts,
-  reqestDeleteContact,
-} from '../service/APIservice';
+// import {
+//   reqestAddContact,
+//   reqestAllContacts,
+//   reqestDeleteContact,
+// } from '../service/APIservice';
 
 import { createSlice,createAsyncThunk} from '@reduxjs/toolkit'
 
 import { fetchContacts, addContact, removeContacts } from '../redux/operations'
-
-export const fetchContacts = createAsyncThunk(
-  'contacts/fetchAll',
-  async (_, thunkAPI) => {
-    try {
-      const contacts = await reqestAllContacts();
-      return contacts;
-    } catch (error) {
-      thunkAPI.rejectWithValue(error.message);
-    }
-  }
-);
-
-export const addContact = createAsyncThunk(
-  'contacts/addContact',
-  async (contact, thunkAPI) => {
-    try {
-      const contacts = await reqestAddContact(contact);
-      return contacts;
-    } catch (error) {
-      thunkAPI.rejectWithValue(error.message);
-    }
-  }
-);
-
-export const removeContacts = createAsyncThunk(
-  'contacts/deleteContact',
-  async (id, thunkAPI) => {
-    try {
-      const contacts = await reqestDeleteContact(id);
-      return contacts;
-    } catch (error) {
-      thunkAPI.rejectWithValue(error.message);
-    }
-  }
-);
 
 const INITIAL_STATE = {
   contacts: {items: [], isLoading: false,
