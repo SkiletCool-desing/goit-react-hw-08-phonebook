8c1341f98cd4d3df91a7bea041d4ed01cf4a49ed
SkiletCool-desing <128517776+SkiletCool-desing@users.noreply.github.com>
2023-12-28 20:25:14 +0200

    phone

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,21 +1,21 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Helmet } from 'react-helmet';
-// import { contactsEditor } from '../components/ContactEditor/ContactEditor';
+import { fetchContacts } from '../redux/operations';
 import { ContactList } from '../components/ContactList/ContactList';
 import { selectLoading } from '../redux/contact/selectors';
 export default function Contacts() {
   const dispatch = useDispatch();
   const isLoading = useSelector(selectLoading);
   useEffect(() => {
-    // dispatch(fetchContactsEditor());
+    dispatch(fetchContacts());
   }, [dispatch]);
   return (
     <>
       <Helmet>
         <title>Your contacts</title>
       </Helmet>
-      <contactsEditor />
+      <ContactList/>
       <div>{isLoading && 'Request in progress...'}</div>
       <ContactList />
     </>
